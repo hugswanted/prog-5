8f3c77e45b166f13a976e2524dc3b1413431b0ca
hugswanted <97693044+hugswanted@users.noreply.github.com>
2022-04-28 11:59:31 -0400

    new

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+
+
+{
+    public List<GameObject> targets;
+    // Start is called before the first frame update
+    private float spawnRate = 1.0f;
+    void Start() { StartCoroutine(SpawnTarget());; }
+    
+    IEnumerator SpawnTarget (){
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
